checked gradients and fixed triton kernels

mone_pytorch/layers.py
@@ -7,6 +7,8 @@ import torch.nn.functional as F
 from ops.nested_linear_triton import nested_linear_expand_triton
 from ops.nested_linear_triton import nested_linear_contract_triton
 
+from typing import Optional
+
 
 XFORMERS_ENABLED = os.environ.get("XFORMERS_DISABLED") is None
 try:
@@ -43,11 +45,11 @@ class NestedLinearExpand(nn.Linear):
     def forward(self, x: torch.Tensor, token_mask: torch.Tensor) -> torch.Tensor:
         if CUDA_AVAILABLE:
             return nested_linear_expand_triton(
-                x, self.weight, self.bias, token_mask, self.num_experts
+                x, self.weight, token_mask, self.bias, self.num_experts
             )
         else:
             return nested_linear_expand(
-                x, self.weight, self.bias, token_mask, self.num_experts
+                x, self.weight, token_mask, self.bias, self.num_experts
             )
 
     def deterministic_init(self):
@@ -89,8 +91,8 @@ class NestedLinearContract(nn.Linear):
 def nested_linear_expand(
     x: torch.Tensor,
     w: torch.Tensor,
-    b: torch.Tensor,
     token_mask: torch.Tensor,
+    b: Optional[torch.Tensor] = None,
     num_experts: int = 4,
 ) -> torch.Tensor:
     batch, seq_len, in_dim = x.shape
@@ -123,8 +125,8 @@ def nested_linear_expand(
 def nested_linear_contract(
     x: torch.Tensor,
     w: torch.Tensor,
-    b: torch.Tensor,
     token_mask: torch.Tensor,
+    b: Optional[torch.Tensor] = None,
     num_experts: int = 4,
 ) -> torch.Tensor:
     batch, seq_len, in_dim = x.shape

mone_pytorch/ops/nested_linear_triton.py
@@ -464,7 +464,7 @@ def nested_linear_expand_dx_kernel(
     acc = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=tl.float32)
     for e_i in range(E):  # e is the number of experts
         # Get in_dim // 2^(e - e_i)
-        n_i = K >> (E - e_i - 1)
+        n_i = N >> (E - e_i - 1)
 
         # relevant offsets for a and b
         # needs to be updated for each expert
@@ -565,7 +565,8 @@ def nested_linear_expand_dw_kernel(
 
     # initialize accumulator
     acc = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=tl.float32)
-    acc_bias = tl.zeros((BLOCK_SIZE_M,), dtype=tl.float32)
+    if dbias_ptr is not None:
+        acc_bias = tl.zeros((BLOCK_SIZE_M,), dtype=tl.float32)
     for e_i in range(E):  # e is the number of experts
         # Get out_dim // 2^(e - e_i)
         n_i = N >> (E - e_i - 1)
@@ -784,7 +785,8 @@ def nested_linear_contract_dw_kernel(
 
     # initialize accumulator
     acc = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=tl.float32)
-    acc_bias = tl.zeros((BLOCK_SIZE_M,), dtype=tl.float32)
+    if dbias_ptr is not None:
+        acc_bias = tl.zeros((BLOCK_SIZE_M,), dtype=tl.float32)
     for e_i in range(E):  # e is the number of experts
         # Get out_dim // 2^(e - e_i)
         m_i = M >> (E - e_i - 1)
@@ -859,7 +861,7 @@ def nested_linear_contract_dw(dyT, x, mask, bias=False, experts=4):
     return dw, dbias
 
 class NestedLinearExpand(torch.autograd.Function):
-    @custom_fwd
+    @custom_fwd(device_type="cuda")
     @staticmethod
     def forward(ctx, x, w, mask, bias=None, experts=4):
         input_shape = x.shape
@@ -877,7 +879,7 @@ class NestedLinearExpand(torch.autograd.Function):
             input_shape
         )
 
-    @custom_bwd
+    @custom_bwd(device_type="cuda")
     @staticmethod
     def backward(ctx, grad_output):
         input_shape = ctx.input_shape
@@ -895,7 +897,7 @@ class NestedLinearExpand(torch.autograd.Function):
         return dx, dw, None, dbias, None
     
 class NestedLinearContract(torch.autograd.Function):
-    @custom_fwd
+    @custom_fwd(device_type="cuda")
     @staticmethod
     def forward(ctx, x, w, mask, bias=None, experts=4):
         input_shape = x.shape
@@ -913,7 +915,7 @@ class NestedLinearContract(torch.autograd.Function):
             input_shape
         )
 
-    @custom_bwd
+    @custom_bwd(device_type="cuda")
     @staticmethod
     def backward(ctx, grad_output):
         input_shape = ctx.input_shape